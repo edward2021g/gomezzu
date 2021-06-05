bfb5dcf35c8c41abc38845e6716329da54d235d5
edward2021g <eduardo6412020@outlook.es>
2021-06-04 23:48:19 -0500

    commit

--- Parcial 3/Ejercicio 3/JsGrid.php
+++ Parcial 3/Ejercicio 3/JsGrid.php
@@ -1,22 +1,41 @@
 <?php
 
-        define('servidor','localhost:3307');
-        define('nombre_bd','gomezzu');
-        define('usuario','root');
-        define('password','root');         
-        
-        try{
-           $conexion = new PDO("mysql:host=".servidor.";dbname=".nombre_bd, usuario, password);            
-            
-            $conexion->query("SET NAMES 'UTF8'");
-            $cadenaConsulta = "SELECT * FROM libros";
-            $consulta = $conexion->prepare($cadenaConsulta);
-            $consulta->execute();
-    
-        
-        $conexion = null;
-        }
-        catch (Exception $e){
-            die("El error de Conexión es :".$e->getMessage());
-        }         
-        ?>
+$hostname='localhost:3307';
+$database='gomezzu';
+$username='root';
+$password='root';
+
+
+try {
+    $con = new PDO("mysql:host=$hostname;dbname=$database",$username,$password);
+} catch(PDOException $e) {
+    $row['resultado']  = '1';
+    $row['informacion']= 'Error DB';
+    $row['mensaje']    = 'Error de conexión a la base de datos';
+    $row['detalle']    = $e->getMessage();
+}
+
+$con->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+
+try {
+    $consultaSql = "select * from libros";
+    $consulta = $con -> prepare($consultaSql);
+    $consulta -> execute();
+    $resultado = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    $consulta->closeCursor();
+
+    $row['resultado']  = '0';
+    $row['informacion']= 'OK';
+    $row['mensaje']    = 'Se leyeron datos';
+    $row['detalle']    = $resultado;
+
+} catch(PDOException $e) {
+    $row['resultado']  = '2';
+    $row['informacion']= 'Error DB';
+    $row['mensaje']    = 'Error de consulta a la base de datos';
+    $row['detalle']    = $e->getMessage();
+}
+
+//echo json_encode($resultado);
+echo json_encode($row);
+?>
